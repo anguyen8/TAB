e923027bcba93e61eaf5692462d3ca7f4d356101
Pooyan <pooyan.rmz@gmail.com>
2024-07-18 16:48:21 -0500

    uploading the model

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
-# Co-attention-Bottleneck
+# CAB
+
+Official implementation of paper Co-Attention Bottleneck: Explainable and Causal Attention Emerged from Transformers Trained to Detect Images Changes
+
+## Requirements
+
+```sh
+conda env create -f env.yml
+conda activate cab
+```
+
+
+## Data Preparing
+
+**For CLEVR-Change**
+
+The official data can be found here: [google drive link](https://drive.google.com/file/d/1HJ3gWjaUJykEckyb2M0MB4HnrJSihjVe/view) provided by [Robust Change Captioning (ICCV19)](https://github.com/Seth-Park/RobustChangeCaptioning). 
+
+Extracting this file will create data directory.
+
+```sh
+tar -xzvf clevr_change.tar.gz
+```
+
+For the convenience, you can also download the three json files from [link](https://drive.google.com/drive/folders/1g8QD6Y3La8cIamE7jeSSlXTw8G3r5Q8o?usp=sharing).
+
+You would get
+
+```
+your_data_path
+|–– clevr_change/
+|   |–– data/
+|   |   |–– images/
+|   |   |–– nsc_images/
+|   |   |–– sc_images/
+|   |   |–– sc_images/
+|   |   |–– change_captions.json
+|   |   |–– no_change_captions.json
+|   |   |–– splits.json
+|   |   |–– type_mapping.json
+```
+
+
+## Pretrained Weight
+
+```sh
+cd ckpts
+mkdir pretrained
+```
+
+You can download the [Pretrained Weights](https://drive.google.com/drive/folders/1qOYVpZy57clJPF6AThsnO0Tfy4zq-gg1?usp=sharing). We use weights from [CLIP4IDC](https://github.com/sushizixin/CLIP4IDC) to initialize the vision encoder (backbone) in CAB.
+
+```
+ckpts
+|–– pretrained/
+|   |–– pytorch_model.bin.clevr
+```
+
+Download CLIP (ViT-B/32 and ViT-B/16) weight,
+```sh
+wget -P ./modules https://openaipublic.azureedge.net/clip/models/40d365715913c9da98579312b702a82c18be219cc2a73407c4526f58eba950af/ViT-B-32.pt
+wget -P ./modules https://openaipublic.azureedge.net/clip/models/5806e77cd80f8b59890b7e101eabd078d9fb84e6937f9e85e4ecb61988df416f/ViT-B-16.pt
+```
+
---
+++ modules/modeling.py
@@ -0,0 +1,225 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import logging
+
+import torch
+from torch import nn
+
+from modules.until_module import PreTrainedModel, AllGather
+from modules.module_clip import CLIP, convert_weights
+from torch.nn.utils.rnn import pad_packed_sequence, pack_padded_sequence
+
+logger = logging.getLogger(__name__)
+allgather = AllGather.apply
+
+class CABPreTrainedModel(PreTrainedModel, nn.Module):
+    """ An abstract class to handle weights initialization and
+        a simple interface for dowloading and loading pretrained models.
+    """
+    def __init__(self, *inputs, **kwargs):
+        super(CABPreTrainedModel, self).__init__()
+        self.clip = None
+
+    @classmethod
+    def from_pretrained(cls, state_dict=None, cache_dir=None, type_vocab_size=2, *inputs, **kwargs):
+
+        task_config = None
+        if "task_config" in kwargs.keys():
+            task_config = kwargs["task_config"]
+            if not hasattr(task_config, "local_rank"):
+                task_config.__dict__["local_rank"] = 0
+            elif task_config.local_rank == -1:
+                task_config.local_rank = 0
+
+        if state_dict is None: state_dict = {}
+        pretrained_clip_name = "ViT-B/32"
+        if hasattr(task_config, 'pretrained_clip_name'):
+            pretrained_clip_name = task_config.pretrained_clip_name
+        clip_state_dict = CLIP.get_config(pretrained_clip_name=pretrained_clip_name)
+        for key, val in clip_state_dict.items():
+            new_key = "clip." + key
+            if new_key not in state_dict:
+                state_dict[new_key] = val.clone()
+
+        model = cls(clip_state_dict, *inputs, **kwargs)
+
+        ## ===> Initialization trick [HARD CODE]
+        if model.linear_patch == "3d":
+            contain_conv2 = False
+            for key in state_dict.keys():
+                if key.find("visual.conv2.weight") > -1:
+                    contain_conv2 = True
+                    break
+            if contain_conv2 is False and hasattr(model.clip.visual, "conv2"):
+                cp_weight = state_dict["clip.visual.conv1.weight"].clone()
+                kernel_size = model.clip.visual.conv2.weight.size(2)
+                conv2_size = model.clip.visual.conv2.weight.size()
+                conv2_size = list(conv2_size)
+
+                left_conv2_size = conv2_size.copy()
+                right_conv2_size = conv2_size.copy()
+                left_conv2_size[2] = (kernel_size - 1) // 2
+                right_conv2_size[2] = kernel_size - 1 - left_conv2_size[2]
+
+                left_zeros, right_zeros = None, None
+                if left_conv2_size[2] > 0:
+                    left_zeros = torch.zeros(*tuple(left_conv2_size), dtype=cp_weight.dtype, device=cp_weight.device)
+                if right_conv2_size[2] > 0:
+                    right_zeros = torch.zeros(*tuple(right_conv2_size), dtype=cp_weight.dtype, device=cp_weight.device)
+
+                cat_list = []
+                if left_zeros != None: cat_list.append(left_zeros)
+                cat_list.append(cp_weight.unsqueeze(2))
+                if right_zeros != None: cat_list.append(right_zeros)
+                cp_weight = torch.cat(cat_list, dim=2)
+
+                state_dict["clip.visual.conv2.weight"] = cp_weight
+
+        ## <=== End of initialization trick
+
+        if state_dict is not None:
+            model = cls.init_preweight(model, state_dict, task_config=task_config)
+
+        return model
+
+def show_log(task_config, info):
+    if task_config is None or task_config.local_rank == 0:
+        logger.warning(info)
+
+def update_attr(target_name, target_config, target_attr_name, source_config, source_attr_name, default_value=None):
+    if hasattr(source_config, source_attr_name):
+        if default_value is None or getattr(source_config, source_attr_name) != default_value:
+            setattr(target_config, target_attr_name, getattr(source_config, source_attr_name))
+            show_log(source_config, "Set {}.{}: {}.".format(target_name,
+                                                            target_attr_name, getattr(target_config, target_attr_name)))
+    return target_config
+
+def check_attr(target_name, task_config):
+    return hasattr(task_config, target_name) and task_config.__dict__[target_name]
+
+class CAB(CABPreTrainedModel):
+    def __init__(self, clip_state_dict, task_config):
+        super(CAB, self).__init__()
+        self.task_config = task_config
+        self.ignore_video_index = -1
+
+        # CLIP Encoders: From OpenAI: CLIP [https://github.com/openai/CLIP] ===>
+        vit = "visual.proj" in clip_state_dict
+        assert vit
+        if vit:
+            vision_width = clip_state_dict["visual.conv1.weight"].shape[0]
+            vision_layers = len(
+                [k for k in clip_state_dict.keys() if k.startswith("visual.") and k.endswith(".attn.in_proj_weight")])
+            vision_patch_size = clip_state_dict["visual.conv1.weight"].shape[-1]
+            grid_size = round((clip_state_dict["visual.positional_embedding"].shape[0] - 1) ** 0.5)
+            image_resolution = vision_patch_size * grid_size
+        else:
+            counts: list = [len(set(k.split(".")[2] for k in clip_state_dict if k.startswith(f"visual.layer{b}"))) for b in
+                            [1, 2, 3, 4]]
+            vision_layers = tuple(counts)
+            vision_width = clip_state_dict["visual.layer1.0.conv1.weight"].shape[0]
+            output_width = round((clip_state_dict["visual.attnpool.positional_embedding"].shape[0] - 1) ** 0.5)
+            vision_patch_size = None
+            assert output_width ** 2 + 1 == clip_state_dict["visual.attnpool.positional_embedding"].shape[0]
+            image_resolution = output_width * 32
+
+        embed_dim = clip_state_dict["text_projection"].shape[1]
+        context_length = clip_state_dict["positional_embedding"].shape[0]
+        vocab_size = clip_state_dict["token_embedding.weight"].shape[0]
+        transformer_width = clip_state_dict["ln_final.weight"].shape[0]
+        transformer_heads = transformer_width // 64
+        transformer_layers = len(set(k.split(".")[2] for k in clip_state_dict if k.startswith(f"transformer.resblocks")))
+
+        show_log(task_config, "\t embed_dim: {}".format(embed_dim))
+        show_log(task_config, "\t image_resolution: {}".format(image_resolution))
+        show_log(task_config, "\t vision_layers: {}".format(vision_layers))
+        show_log(task_config, "\t vision_width: {}".format(vision_width))
+        show_log(task_config, "\t vision_patch_size: {}".format(vision_patch_size))
+        show_log(task_config, "\t context_length: {}".format(context_length))
+        show_log(task_config, "\t vocab_size: {}".format(vocab_size))
+        show_log(task_config, "\t transformer_width: {}".format(transformer_width))
+        show_log(task_config, "\t transformer_heads: {}".format(transformer_heads))
+        show_log(task_config, "\t transformer_layers: {}".format(transformer_layers))
+
+        self.linear_patch = '2d'
+        if hasattr(task_config, "linear_patch"):
+            self.linear_patch = task_config.linear_patch
+            show_log(task_config, "\t\t linear_patch: {}".format(self.linear_patch))
+
+        # use .float() to avoid overflow/underflow from fp16 weight. https://github.com/openai/CLIP/issues/40
+        cut_top_layer = 0
+        show_log(task_config, "\t cut_top_layer: {}".format(cut_top_layer))
+        self.clip = CLIP(
+            embed_dim,
+            image_resolution, vision_layers-cut_top_layer, vision_width, vision_patch_size,
+            context_length, vocab_size, transformer_width, transformer_heads, transformer_layers-cut_top_layer,
+            linear_patch=self.linear_patch, intra_layers=task_config.intra_num_hidden_layers
+        ).float()
+
+        bert_word_embeddings_weight = self.clip.token_embedding.weight
+        bert_position_embeddings_weight = self.clip.positional_embedding
+
+        for key in ["input_resolution", "context_length", "vocab_size"]:
+            if key in clip_state_dict:
+                del clip_state_dict[key]
+
+        convert_weights(self.clip)
+        # <=== End of CLIP Encoders
+
+        self.linear_layer = nn.Linear(1024, 1)
+        self.activation = nn.Sigmoid()
+        self.BCEloss = nn.BCELoss()
+        self.CSloss = nn.CosineSimilarity(dim=1, eps=1e-6)
+
+        self.apply(self.init_weights)
+
+    def forward(self, bef_image, aft_image, target, image_mask=None):
+                    
+        image_mask = image_mask.view(-1, image_mask.shape[-1])
+        image = torch.cat([bef_image, aft_image], 1)
+        b, pair, channel, h, w = image.shape
+        image = image.view(b * pair, channel, h, w)
+
+        visual_emb, visual_output = self.get_visual_output(image, image_mask, shaped=True, video_frame=pair)
+
+        if self.training:
+            
+            loss = 0.
+            decision, label = self.predict_similarity(visual_emb, target)
+            loss += self.BCEloss(decision.float(), label.float())
+                
+            return loss
+        else:
+            return None
+
+    def predict_similarity(self, visual_output, label):
+        visual_output = visual_output.contiguous()
+        if self.training:
+            visual_output = allgather(visual_output, self.task_config)
+            label = allgather(label, self.task_config)
+            torch.distributed.barrier()
+            
+        visual_output = visual_output.squeeze(1)
+
+        hidden = self.linear_layer(visual_output)
+        hidden = self.activation(hidden)
+
+        return output.squeeze(1), label
+
+    def get_visual_output(self, video, visual_mask, shaped=False, video_frame=-1):
+        if shaped is False:
+            visual_mask = visual_mask.view(-1, visual_mask.shape[-1])
+            video = torch.as_tensor(video).float()
+            b, pair, channel, h, w = video.shape
+            video = video.view(b * pair, channel, h, w)
+            video_frame = pair
+
+        bs_pair = visual_mask.size(0)
+        visual_hidden, visual_output = self.clip.encode_image(video, video_frame=video_frame, return_hidden=True)
+        visual_hidden = visual_hidden.float()
+        visual_output = visual_output.float()
+        visual_hidden = visual_hidden.view(bs_pair, -1, visual_hidden.size(-1))
+
+        return visual_hidden, visual_output
